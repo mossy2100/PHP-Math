2963d4a83e8e04831a894d9ae35c996b788a983d
Shaun Moss <shaun@astromultimedia.com>
2025-10-16 12:24:42 +1000

    Initial commit.

---
+++ tests/ComplexTests.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Galaxon\Math\Tests;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Superclasses\Math\src\Complex;
+
+#[CoversClass(Complex::class)]
+final class ComplexTests extends TestCase
+{
+}
