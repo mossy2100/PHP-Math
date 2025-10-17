8c6fe60f020997acfdf4ebc5496e21baa9e5c425
Shaun Moss <shaun@astromultimedia.com>
2025-10-17 10:53:26 +1000

    Reduced to just the new Math functions.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PHP-Math
 
+This library contains functions for mathematical operations.
+
+It's a general purpose library used by other packages in the Galaxon PHP family.
+
+The naming pattern generally follows the core Math functions in PHP.
+
+See: https://www.php.net/manual/en/ref.math.php
---
+++ src/Math.php
@@ -0,0 +1,303 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Galaxon\Math;
+
+use InvalidArgumentException;
+use OverflowException;
+use Random\RandomException;
+
+// region Inspection methods
+
+/**
+ * Check if a value is a number, i.e. an integer or a float.
+ * This varies from is_numeric(), which also returns true for numeric strings.
+ *
+ * @param mixed $value The value to check.
+ * @return bool True if the value is a number, false otherwise.
+ */
+function is_number(mixed $value): bool
+{
+    return is_int($value) || is_float($value);
+}
+
+/**
+ * Check if a value is an unsigned integer.
+ *
+ * @param mixed $value The value to check.
+ * @return bool True if the value is an unsigned integer, false otherwise.
+ */
+function isUnsignedInt(mixed $value): bool
+{
+    return is_int($value) && $value >= 0;
+}
+
+/**
+ * Determines if a floating-point number is negative zero (-0.0).
+ *
+ * In IEEE-754 floating-point arithmetic, negative zero is a distinct value from
+ * positive zero, though they compare as equal. This method provides a way to
+ * distinguish between them.
+ *
+ * The method works by dividing 1.0 by the input value. For negative zero,
+ * this division results in negative infinity (-INF).
+ *
+ * @param float $x The floating-point number to check.
+ * @return bool True if the value is negative zero (-0.0), false otherwise.
+ */
+function isNegativeZero(float $x): bool
+{
+    // Using fdiv() to avoid a division by zero error.
+    return $x == 0.0 && fdiv(1.0, $x) === -INF;
+}
+
+/**
+ * Determines if a floating-point number is positive zero (+0.0).
+ *
+ * In IEEE-754 floating-point arithmetic, positive zero is a distinct value from
+ * negative zero, though they compare as equal. This method provides a way to
+ * distinguish between them.
+ *
+ * The method works by dividing 1.0 by the input value. For positive zero,
+ * this division results in positive infinity (INF).
+ *
+ * @param float $x The floating-point number to check.
+ * @return bool True if the value is positive zero (+0.0), false otherwise.
+ */
+function isPositiveZero(float $x): bool
+{
+    // Using fdiv() to avoid a division by zero error.
+    return $x == 0.0 && fdiv(1.0, $x) === INF;
+}
+
+/**
+ * Check if a number is negative.
+ *
+ * NB: This method returns true for the IEEE special values -0.0 (negative zero) and -INF (negative infinity).
+ * It returns false for +0.0, +INF, and NaN.
+ *
+ * @param float $value The value to check.
+ * @return bool True if the value is negative, false otherwise.
+ */
+function isNegative(float $value): bool
+{
+    return !is_nan($value) && ($value < 0 || self::isNegativeZero($value));
+}
+
+/**
+ * Check if a number is positive.
+ *
+ * NB: This method returns true for the IEEE special values 0.0 (positive zero) and INF (positive infinity).
+ * It returns false for -0.0, -INF, and NaN.
+ *
+ * @param float $value The value to check.
+ * @return bool True if the value is positive, false otherwise.
+ */
+function isPositive(float $value): bool
+{
+    return !is_nan($value) && ($value > 0 || self::isPositiveZero($value));
+}
+
+// endregion
+
+// region Sign methods
+
+/**
+ * Sign function.
+ *
+ * @param int|float $value The number whose sign to check.
+ * @param bool $zeroForZero If true (default), returns 0 for zero; otherwise, return the sign of the zero.
+ * @return int 1 if the number is positive, -1 if negative, and 0, 1, or -1 (depending on second argument) if 0.
+ */
+function sign(int|float $value, bool $zeroForZero = true): int
+{
+    // Check for positive.
+    if ($value > 0) {
+        return 1;
+    }
+
+    // Check for negative.
+    if ($value < 0) {
+        return -1;
+    }
+
+    // Return result for 0.
+    return $zeroForZero ? 0 : (self::isNegativeZero($value) ? -1 : 1);
+}
+
+/**
+ * Copy the sign of one number to another.
+ *
+ * @param float $num The number to copy the sign to.
+ * @param float $sign_source The number to copy the sign from.
+ * @return float The number with the sign of $sign_source.
+ */
+function copySign(float $num, float $sign_source): float
+{
+    // Guard. This method is only valid for numbers.
+    if (is_nan($num) || is_nan($sign_source)) {
+        throw new InvalidArgumentException("NaN is not allowed for either parameter.");
+    }
+
+    return abs($num) * self::sign($sign_source, false);
+}
+
+// endregion
+
+// region Integer arithmetic
+
+/**
+ * Add two integers with overflow check.
+ *
+ * @param int $a The first integer.
+ * @param int $b The second integer.
+ * @return int The added integers if no overflow occurs.
+ * @throws OverflowException If the addition results in overflow.
+ */
+function intAdd(int $a, int $b): int
+{
+    // Do the addition.
+    $c = $a + $b;
+
+    // Check for overflow.
+    if (is_float($c)) {
+        throw new OverflowException("Overflow in integer addition.");
+    }
+
+    // Return the result.
+    return $c;
+}
+
+/**
+ * Subtract one integer from another with overflow check.
+ *
+ * @param int $a The first integer.
+ * @param int $b The second integer.
+ * @return int The result of subtracting the second integer from the first, if no overflow occurs.
+ * @throws OverflowException If the subtraction results in overflow.
+ */
+function intSub(int $a, int $b): int
+{
+    // Do the subtraction.
+    $c = $a - $b;
+
+    // Check for overflow.
+    if (is_float($c)) {
+        throw new OverflowException("Overflow in integer subtraction.");
+    }
+
+    // Return the result.
+    return $c;
+}
+
+/**
+ * Multiply two integers with overflow check.
+ *
+ * @param int $a The first integer.
+ * @param int $b The second integer.
+ * @return int The multiplied integers if no overflow occurs.
+ * @throws OverflowException If the multiplication results in overflow.
+ */
+function intMul(int $a, int $b): int
+{
+    // Do the multiplication.
+    $c = $a * $b;
+
+    // Check for overflow.
+    if (is_float($c)) {
+        throw new OverflowException("Overflow in integer multiplication.");
+    }
+
+    // Return the result.
+    return $c;
+}
+
+/**
+ * Raise one integer to the power of another with overflow check.
+ *
+ * @param int $a The first integer.
+ * @param int $b The second integer. Must be non-negative.
+ * @return int The result integer if no overflow occur.
+ * @throws InvalidArgumentException If $b is negative.
+ * @throws OverflowException If the exponentiation results in overflow.
+ */
+function intPow(int $a, int $b): int
+{
+    // Handle b < 0.
+    if ($b < 0) {
+        throw new InvalidArgumentException("Negative exponents are not supported.");
+    }
+
+    // Do the exponentiation.
+    $c = $a ** $b;
+
+    // Check for overflow.
+    if (is_float($c)) {
+        throw new OverflowException("Overflow in exponentiation.");
+    }
+
+    // Return the result.
+    return $c;
+}
+
+// endregion
+
+// region Other methods
+
+/**
+ * Calculate the greatest common divisor of two or more integers.
+ *
+ * @param array $nums The integers to calculate the GCD of.
+ * @return int The greatest common divisor.
+ */
+function gcd(int ...$nums): int
+{
+    // Guard.
+    if (count($nums) === 0) {
+        throw new InvalidArgumentException("At least one number is required.");
+    }
+
+    // Initialise to first number.
+    $result = abs($nums[0]);
+
+    // Calculate the GCD using Euclid's algorithm.
+    for ($i = 1; $i < count($nums); $i++) {
+        $a = $result;
+        $b = abs($nums[$i]);
+
+        while ($b !== 0) {
+            $temp = $b;
+            $b = $a % $b;
+            $a = $temp;
+        }
+
+        $result = $a;
+    }
+
+    return $result;
+}
+
+// endregion
+
+// region Random numbers
+
+/**
+ * Generate a random float that is not NaN, positive or negative infinity, or negative zero.
+ *
+ * @return float The random float.
+ * @throws RandomException If an appropriate source of randomness cannot be found.
+ */
+function randFloat(): float
+{
+    // Generate random floats until a valid value is found.
+    // It's very unlikely the loop will repeat more than once or twice.
+    do {
+        $bytes = random_bytes(8);
+        $float = unpack('d', $bytes)[1];
+    } while (!is_finite($float) || self::isNegativeZero($float));
+
+    return $float;
+}
+
+// endregion
